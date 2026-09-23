939917b776e925e40154cd161a4aaf25b2afb370
Lo-lo78 <vmanolo301@gmail.com>
2026-09-23 15:39:39 +0200

    Optimize sequencer LArp parameter locks and local filter CPU usage

--- Source/ScreenReaderAnnouncer.h
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#pragma once
-#include <juce_gui_basics/juce_gui_basics.h>
-
-namespace ljuno
-{
-void announceToActiveScreenReader (juce::Component&, const juce::String&);
-}
